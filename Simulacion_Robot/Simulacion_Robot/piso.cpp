49bacef53fc9ee31f8277bd5c77bbe9ccc985192
Joaquin Gaytan <jgaytan@itba.edu.ar>
2018-08-15 22:44:59 -0300

    Commit inicial

---
+++ Simulacion_Robot/Simulacion_Robot/piso.cpp
@@ -0,0 +1,93 @@
+#include "piso.h"
+#include <cstdlib>
+#include <cmath>
+
+
+
+piso::
+piso(unsigned int h, unsigned int w)
+{
+	hcount = h; // inicializo cantidad de tiles
+	wcount = w; 
+	baldosas = (bool*)malloc(h*w * sizeof(bool)); // reservo memoria para el arreglo
+
+	if (baldosas != NULL)
+	{
+		for (int i = 0; i < w*h; i++) // inicializo baldosas en sucias
+		{
+			baldosas[i] = DIRTY;
+		}
+
+		error.err_code = NO_ERR;
+	}
+
+	else
+	{
+		error.err_code = MEM_ERR;
+		error.description = "Error en la alocación de memoria!"
+	}
+
+}
+
+bool piso::
+isDirty(unsigned int x, unsigned int y)
+{
+	if ((x<wcount) && (y<hcount)) // valido coordenadas
+	{
+		return (baldosas[x + y * wcount]);
+	}
+
+}
+
+bool piso::
+isClean(void)
+{
+	bool res = CLEAN;
+	for (int i = 0; i < hcount*wcount; i++)
+	{
+		if (baldosas[i] = DIRTY)
+		{
+			res = DIRTY;
+		}
+	}
+	return res;
+}
+
+void piso::
+cleanTile(unsigned int x, unsigned int y)
+{
+	if ((x<wcount) && (y < hcount))
+	{
+		baldosas[x + y * wcount] = CLEAN;
+	}
+}
+
+void piso::
+cleanTile(position_t pos)
+{
+	unsigned int x = floor(pos.x);
+	unsigned int y = floor(pos.y);
+
+	if ((x < wcount) &&  (y< hcount))
+	{
+		baldosas[x + y * wcount] = CLEAN;
+	}
+}
+
+void piso::
+destroyFloor(void)
+{
+	free((void*)baldosas);
+}
+
+unsigned int piso::
+getHeight(void)
+{
+	return(hcount);
+}
+
+unsigned int piso::
+getWidth(void)
+{
+	return(wcount);
+}
---
+++ Simulacion_Robot/Simulacion_Robot/piso.h
@@ -0,0 +1,35 @@
+#include <string>
+
+#define CLEAN true;
+#define DIRTY false;
+
+typedef enum error{NO_ERR,MEM_ERR} errtype_t;
+
+typedef struct
+{
+	unsigned int x;
+	unsigned int y;
+}position_t;
+
+typedef struct
+{
+	string description;
+	errtype_t err_code; // agregar typedef de enum ???
+}error_t;
+
+class piso
+{
+	bool* baldosas;
+	error_t error;
+	unsigned int hcount;
+	unsigned int wcount;
+public:
+	piso(unsigned int h, unsigned int w);
+	bool isDirty(unsigned int x, unsigned int y);
+	bool isClean(void);
+	void cleanTile(unsigned int x, unsigned int y);
+	void cleanTile(position_t pos);
+	void destroyFloor(void);
+	unsigned int getHeight(void);
+	unsigned int getWidth(void);
+};
